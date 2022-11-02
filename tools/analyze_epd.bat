a3a32282c539e41513170f0870efb42a7ec8d52c
fsmosca <fsmosca@users.noreply.github.com>
2022-11-02 22:26:30 +0800

    Update analyze_epd.bat

--- tools/analyze_epd.bat
+++ tools/analyze_epd.bat
@@ -1,4 +1,7 @@
-set engine="F:\Chess\Engines\stockfish\stockfish_15/stockfish_15_modern.exe"
+:: This batch file is used when analyzing a single epd.
+
+
+set engine="F:/Chess/Engines/stockfish/stockfish_15/stockfish_15_modern.exe"
 set hash=256
 set threads=1
 set depth=36
@@ -6,7 +9,7 @@ set depth=36
 set username=ferdy
 set ename=sf15
 
-:: The i or index number from google sheet depends on the epd value
+:: The i or index number from google sheet sts_positions, depends on the epd value
 :: If you change the epd, also change the index.
 set i=170
 set epd="8/3q1pk1/3p4/2pB2p1/P2n4/1P4BP/6P1/4R1K1 b - -"
