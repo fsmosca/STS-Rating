36111b40c397ed8d8fa1a510186cfd8668397d2c
fsmosca <fsmosca@users.noreply.github.com>
2022-11-02 22:24:17 +0800

    Create analyze_epd.bat

---
+++ tools/analyze_epd.bat
@@ -0,0 +1,22 @@
+set engine="F:\Chess\Engines\stockfish\stockfish_15/stockfish_15_modern.exe"
+set hash=256
+set threads=1
+set depth=36
+
+set username=ferdy
+set ename=sf15
+
+:: The i or index number from google sheet depends on the epd value
+:: If you change the epd, also change the index.
+set i=170
+set epd="8/3q1pk1/3p4/2pB2p1/P2n4/1P4BP/6P1/4R1K1 b - -"
+
+set out=index_%i%_d%depth%_%username%_%ename%.csv
+
+python analyze.py --epd %epd% ^
+--engine %engine% ^
+--hash-mb %hash% ^
+--threads %threads% ^
+--depth %depth% ^
+--output %out% ^
+--log-file "log_index_%i%_d%depth%_%username%.txt"
