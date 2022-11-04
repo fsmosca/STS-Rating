5c46220dec0ea551b06c62ad5c5f76a2913aa307
fsmosca <fsmosca@users.noreply.github.com>
2022-11-04 14:06:15 +0800

    Add --move-time-sec option
    * If --move-time-sec is defined, the search will be terminated based on depth and move time. If the depth is reached first and there is still time left, then the search will continue until move time is spent. If the depth is not yet reached but there is no more time left, then the search info will be
    saved and search will be terminated.
    * Refactor code.
    * Modify logging formats.
    * Silence some pylint warnings.

--- tools/analyze.py
+++ tools/analyze.py
@@ -5,13 +5,10 @@ Save the analysis to csv file.
 Requirements:
   pip install chess
   pip install pandas
-
-Example:
-  python analyze.py --epd "1kr5/3n4/q3p2p/p2n2p1/PppB1P2/5BP1/1P2Q2P/3R2K1 w - -" --engine "sf15.exe" --hash-mb 512 --threads 1 --depth 24 --output index_1_d24_ferdy_sf15.csv
 """
 
 
-__version__ = '0.6'
+__version__ = '0.7'
 
 
 import argparse
@@ -25,11 +22,13 @@ import chess.engine
 import pandas as pd
 
 
-alphabet = string.ascii_lowercase + string.digits
+ALPHABET = string.ascii_lowercase + string.digits
 
 
-def get_random_index():
-    return ''.join(secrets.choice(alphabet) for _ in range(8))
+def get_random_index() -> str:
+    """Generates a string as index number.
+    """
+    return ''.join(secrets.choice(ALPHABET) for _ in range(8))
 
 
 def set_engine_options(engine, engine_option, hashmb, threads):
@@ -38,18 +37,18 @@ def set_engine_options(engine, engine_option, hashmb, threads):
     is_hash_set, is_thread_set = False, False
     if engine_option is not None:
         opt_list = engine_option.split(',')
-        for o in opt_list:
-            opt = o.strip()
+        for optv in opt_list:
+            opt = optv.strip()
             name = opt.split('=')[0].strip()
             value = opt.split('=')[1].strip()
 
             if name not in engine.options:
-                logging.warning(f'option {name} is not supported by the engine')
+                logging.warning('option %s is not supported by the engine', name)
                 continue
 
             engine.configure({name: value})
-            logging.debug(f'{name} is set to {value}')
-            
+            logging.debug('%s is set to %s', name, value)
+
             if name.lower() == 'hash':
                 is_hash_set = True
             if name.lower() == 'threads':
@@ -61,28 +60,56 @@ def set_engine_options(engine, engine_option, hashmb, threads):
         engine.configure({'Threads': threads})
 
 
-def read_epd_index():
+def read_epd_index() -> dict:
     """Reads json file and return as a dict.
 
     analyze.py and epd_index.json should be in same folder.
     """
-    with open('epd_index.json') as f:
-        data = json.load(f)
+    with open('epd_index.json', encoding='utf-8') as handle:
+        data = json.load(handle)
         return data
 
 
-def get_epd(epd_file):
+def get_epd(epd_file) -> list:
+    """Converts epd's in epd file into a list."""
     epds = []
-    with open(epd_file) as f:
-        for lines in f:
+    with open(epd_file, encoding='utf-8') as handle:
+        for lines in handle:
             epd_line = lines.rstrip()
             epds.append(epd_line)
 
     return epds
 
 
-def analyze(enginefn, epd, epd_file, depth, hashmb, threads,
-            multipv, engine_option, username):
+def get_analysis_data(engine_info, num_moves, epd, engine_name) -> list:
+    """Gets engine analysis.
+    """
+    data = []
+    for i in range(num_moves):
+        move = engine_info[i]['pv'][0]
+        eval_ = engine_info[i]['score'].relative.score(mate_score=32000)
+        depth = engine_info[i]['depth']
+
+        pv = engine_info[i]['pv'][0:7]
+        pv1 = [a.uci() for a in pv]
+        pv2 = ' '.join(pv1)
+
+        data.append([epd, move.uci(), eval_, depth, pv2, engine_name])
+
+    return data
+
+
+def analyze(enginefn: str, epd: str, epd_file: str, depth: int,
+            hashmb: int, threads: int, multipv: int,
+            engine_option: str, username: str, move_time_sec: int):
+    """Analyzes the epd or positions in the epd file.
+
+    Position will be analyzed by an engine run at multipv 10. The
+    best moves, eval, depth, pv and engine name will be saved in csv file.
+
+    Args:
+      enginefn: The engine filename or path/filename.
+    """
     engine = chess.engine.SimpleEngine.popen_uci(enginefn)
     engine_name = engine.id['name']
     set_engine_options(engine, engine_option, hashmb, threads)
@@ -97,35 +124,48 @@ def analyze(enginefn, epd, epd_file, depth, hashmb, threads,
     else:
         epds = get_epd(epd_file)
 
-    for epd in epds:
+    for pos in epds:
         try:
-            index_num = epi[epd]
+            index_num = epi[pos]
         except KeyError:
-            logging.warning(f'Position {epd} is not in epd_index.json. Temp index will be used.')
+            logging.warning('Position %s is not in epd_index.json. Temp index will be used.', pos)
             index_num = get_random_index()
 
-        logging.info(f'epd: {epd}, index: {index_num}')
-        print(f'epd: {epd}, index: {index_num}')
+        logging.info('epd: %s, index: %s', pos, str(index_num))
+        print(f'epd: {pos}, index: {index_num}')
 
-        board = chess.Board(epd)
+        board = chess.Board(pos)
+        num_moves = min(multipv, board.legal_moves.count())
+        adepth = 0  # for csv filename depth info
 
         time_start = time.perf_counter()
 
-        engine_info = engine.analyse(board, limit=limit, multipv=multipv)
+        if move_time_sec is None:
+            engine_info = engine.analyse(board, limit=limit, multipv=multipv)
+            data = get_analysis_data(engine_info, num_moves, pos, engine_name)
+            adepth = depth
+        else:
+            with engine.analysis(board, multipv=multipv) as analysis:
+                for engine_info in analysis:
 
-        data = []
+                    # If depth is already reached, we abort if there is no time.
+                    if engine_info.get("depth", 0) >= depth:
 
-        # Read engine info.
-        for i in range(min(multipv, board.legal_moves.count())):
-            m = engine_info[i]['pv'][0]
-            s = engine_info[i]['score'].relative.score(mate_score=32000)
-            d = engine_info[i]['depth']
+                        # If there is still time, continue the search.
+                        time_left = move_time_sec - (time.perf_counter() - time_start)
+                        if time_left < 0:
+                            analysis.stop()
 
-            p = engine_info[i]['pv'][0:7]
-            p1 = [a.uci() for a in p]
-            p2 = ' '.join(p1)
+                    # Else if depth is not yet reached, but time is, then abort the search.
+                    else:
+                        time_left = move_time_sec - (time.perf_counter() - time_start)
+                        if time_left < 0:
+                            analysis.stop()
 
-            data.append([epd, m.uci(), s, d, p2, engine_name])
+                data = get_analysis_data(analysis.multipv, num_moves, pos, engine_name)
+
+                # Get the depth of top 1 move for csv output filename.
+                adepth = analysis.multipv[0]['depth']
 
         time_end = time.perf_counter()
         print(f'elapse (sec): {time_end - time_start:0.1f}')
@@ -134,7 +174,7 @@ def analyze(enginefn, epd, epd_file, depth, hashmb, threads,
         df.columns = ['epd', 'move', 'eval', 'depth', 'pv', 'engine']
 
         # Build an output filename.
-        output = f'index_{index_num}_d{depth}_{username}.csv'
+        output = f'index_{index_num}_d{adepth}_{username}.csv'
         df.to_csv(output, index=False)
 
     engine.quit()
@@ -149,7 +189,8 @@ def main():
                         help='The position in epd format that will be analyzed, '
                         'e.g --epd "1kr5/3n4/q3p2p/p2n2p1/PppB1P2/5BP1/1P2Q2P/3R2K1 w - -"')
     group.add_argument('--epd-file', type=str,
-                        help='The file that contains epd to be analyzed, e.g. --epd-file sample.epd')
+                        help='The file that contains epd to be analyzed, '
+                        'e.g. --epd-file sample.epd')
 
     parser.add_argument('--username', required=True, type=str,
                         help='username to be used in the output filename, (required).')
@@ -161,6 +202,13 @@ def main():
                         help='The engine number of threads to use (not required, default=1).')
     parser.add_argument('--depth', required=False, type=int, default=20,
                         help='The analysis depth (not required, default=20).')
+    parser.add_argument(
+        '--move-time-sec',
+        help='Analysis time in seconds. If this is not specified, the analysis will be '
+             'based on the depth. If this is specified, the analysis will be terminated based '
+             'on depth and this option. If the depth is reached first and we still have time '
+             'then continue the analysis until move time is reached. If depth is not yet reached '
+             'and time is already reached, then save and abort the analysis')
     parser.add_argument('--multipv', required=False, type=int, default=10,
                         help='The multipv value the engine will be run (not required, default=10).')
     parser.add_argument('--log-file', required=False, type=str,
@@ -174,11 +222,19 @@ def main():
 
     if args.log_file is not None:
         fmode = 'a' if args.epd_file is not None else 'w'
-        logging.basicConfig(level=logging.DEBUG, filename=args.log_file, filemode=fmode)
+        logging.basicConfig(
+            level=logging.DEBUG,
+            filename=args.log_file,
+            filemode=fmode,
+            format='%(levelname)s : %(filename)s: %(message)s')
+
+    move_time_sec = args.move_time_sec
+    if move_time_sec is not None:
+        move_time_sec = int(move_time_sec)
 
     analyze(args.engine, args.epd, args.epd_file, args.depth,
             args.hash_mb, args.threads, args.multipv,
-            args.engine_option, args.username)
+            args.engine_option, args.username, move_time_sec)
 
 
 if __name__ == '__main__':
